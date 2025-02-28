a40c39c1488e9d35db893a6c22b1f8807ab3888b
Vinicius Ferrer <viniciusmacedo581@gmail.com>
2025-02-28 09:22:09 -0400

    formating currency

---
+++ script.js
@@ -0,0 +1,58 @@
+// Cotação das moedas
+const USD = 5.16
+const EUR = 5.68
+const GBP = 6.09
+
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector ("main footer")
+const description = document.getElementById("description")
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// Capturando o evento de submit (enviar) do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break;
+  }
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Exibindo a cotação da moeda selecionada
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    // Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela
+    footer.classList.remove("show-result")
+
+    console.log(error)
+    alert("Não foi possível converter, tente novamente.")
+  }
+}
+// Formata a moeda em Real Brasileiro
+function formatCurrencyBRL(value) {
+  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL.
+  return Number(value).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
