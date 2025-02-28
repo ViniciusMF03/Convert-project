7133977b3775fac1cf3d30c74606f73a4fa9ca09
Vinicius Ferrer <viniciusmacedo581@gmail.com>
2025-02-28 09:37:10 -0400

    formating total

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector ("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -37,6 +38,14 @@ function convertCurrency(amount, price, symbol) {
   try {
     // Exibindo a cotação da moeda selecionada
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    //Calcula o total
+    let total = amount * price
+    // Formata o valor total
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    //Exibe o resultado total
+    result.textContent = `${total} Reais`
 
     // Aplica a classe que exibe o footer para mostrar o resultado
     footer.classList.add("show-result")
